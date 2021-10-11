6b7a3e1f8a0a8543a9448bb719251931cfcee5bf
Edgar <edgar.duronr@gmail.com>
2021-10-11 17:54:35 +0800

    Flip Flap Assignment for SOA Completed

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -2,13 +2,31 @@
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
-  def take_tsv(tsv)
 
+  def take_tsv(tsv)
+    lines = tsv.split("\n")
+    keys = lines[0].split("\t")
+    @data = []
+
+    lines.shift
+    lines.each do |line|
+      input = line.split(/\t/)
+      @data << Hash[keys.zip input]
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
-  def to_tsv
 
+  def lines(value)
+    value.join("\t") + "\n"
+  end
+
+  def to_tsv
+    out = lines(@data[0].keys)
+    @data.each do |i|
+      out << lines(i.values)
+    end
+    out
   end
 end
